0d7b434d185691d9043f8058c4a6d2ce4a5bac27
Diego Sanchez <diegosandmg@gmail.com>
2020-05-22 18:04:16 -0400

    feat: 404 page and contact me dialog on about page

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
-
+import {MatDialog} from '@angular/material/dialog';
+import {DialogContent} from '../../app.component';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
@@ -10,10 +11,15 @@ export class AboutComponent implements OnInit {
 
   constructor(
     private titleService: Title,
+    public dialog: MatDialog,
   ) { }
 
   ngOnInit(): void {
     this.titleService.setTitle('About | Diego Sánchez');
+  }
+
+  openDialog() {
+    this.dialog.open(DialogContent);
   }
 
 }
