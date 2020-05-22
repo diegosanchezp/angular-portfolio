1898afc9879bb91c3c9a6d30a14f14f567b3e794
Diego Sanchez <diegosandmg@gmail.com>
2020-05-21 21:19:14 -0400

    feat: Responsive sidenav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import {BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { faLinkedin, faGithubSquare} from '@fortawesome/free-brands-svg-icons';
+import {faEnvelope} from '@fortawesome/free-solid-svg-icons'
+import {MatDialog} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +12,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-portfolio';
+
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    public dialog: MatDialog){
+  }
+  faEnvelope = faEnvelope
+  // TODO: add router link -->
+  icons = [
+    {icon: faGithubSquare, title: 'Github Account', href: 'https://github.com/diegosanchezp/'},
+    {icon: faLinkedin, title: 'LinkedIn Profile', href: 'https://www.linkedin.com/in/diego-s%C3%A1nchez-b0753b137/'}
+  ]
+  iconSize = '3x'
+  drawerMode = 'side';
+  subsObs: Subscription;
+  openDialog() {
+    const dialogRef = this.dialog.open(DialogContent);
+  }
+  ngOnInit(){
+    this.subsObs = this.breakpointObserver.observe([
+      Breakpoints.Tablet, Breakpoints.Medium, Breakpoints.XSmall
+    ]).subscribe((media)=>{
+      this.drawerMode = media.matches ? "over": "side"
+    });
+  }
+  ngOnDestroy(){
+    this.subsObs.unsubscribe();
+  }
 }
+
+@Component({
+  selector: 'dialog-content',
+  template: `
+    <div mat-dialog-content>
+      <p>Send me an email at</p>
+      <p><b>diegosandmg@gmail.com</b></p>
+    </div>
+
+    <div mat-dialog-actions align="end">
+      <button mat-button mat-dialog-close>Ok</button>
+    </div>
+  `
+})
+export class DialogContent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent, DialogContent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSliderModule } from '@angular/material/slider';
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {MatSidenavModule} from '@angular/material/sidenav'; 
+import {MatButtonModule} from '@angular/material/button'; 
+import {LayoutModule} from '@angular/cdk/layout'; 
+import {MatIconModule} from '@angular/material/icon'; 
+import {MatListModule} from '@angular/material/list'; 
+import {MatRippleModule} from '@angular/material/core'; 
+import {MatDialogModule} from '@angular/material/dialog';
+import { AboutComponent } from './pages/about/about.component'; 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DialogContent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSliderModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    LayoutModule,
+    MatListModule,
+    MatRippleModule,
+    FontAwesomeModule,
+    MatDialogModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
